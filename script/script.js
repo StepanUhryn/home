3eaab428f38eec9e46c377b2d1e106ff14848984
StepanUhryn <71820440+StepanUhryn@users.noreply.github.com>
2021-04-07 11:39:34 +0300

    Домашка 1

--- script/script.js
+++ script/script.js
@@ -1,2 +1,24 @@
-let d = 4444;
-console.log(d);
+const numberOfFilms = +prompt('Скільки фільмів Ви вже подивилися?', '');
+console.log(numberOfFilms, typeof(numberOfFilms));
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+console.log(personalMoviesDB.count);
+console.log(personalMoviesDB['count'])
+
+
+const a = prompt('Один з останніх переглянутих фільмів?',''),
+      b = prompt('На скільки його оцінюєте?', ''),
+      c = prompt('Один з останніх переглянутих фільмів?',''),
+      d = prompt('На скільки його оцінюєте?', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
