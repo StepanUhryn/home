c631da9f3b20c4f04b48ee0ad17dd27b499d168f
StepanUhryn <71820440+StepanUhryn@users.noreply.github.com>
2021-04-07 16:33:48 +0300

    Цикли і умови

--- script/script.js
+++ script/script.js
@@ -1,28 +1,39 @@
 'use strict';
 
-let num = 50;
 
-//Цикли 1.
-/*
-while (num <= 55) {
-    console.log(num);
-    num++;
+const numberOfFilms = +prompt('Скільки фільмів Ви вже подивилися?', '');
+
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+if (personalMoviesDB.count < 10) {
+    console.log('переглянуто досить мало фільмів.');
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log('Ви класичний глядач.');
+}   else if (personalMoviesDB.count >= 30) {
+    console.log('Ви кіноман!');
+}   else {
+        console.log('Сталася помилка!');
 }
-*/
-// Цикли 2
-/*
-do {
-    console.log(num);
-    num++;
+
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один з останніх переглянутих фільмів?',''),
+         b = prompt('На скільки його оцінюєте?', '');
+         if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+             personalMoviesDB.movies[a] = b;
+
+         } else {
+             console.log('error');
+             i--;
+         }
+
 }
-while (num <= 55);*/
 
-// Цикли 3
+console.log(personalMoviesDB);
 
-  for (let i = 1; i <= 10; i++) {
-      if (i === 6) {
-          //break;
-          continue;
-      }
-      console.log(i);
-  }
+
