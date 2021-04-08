3af4b7c15aa00f87a43f0ff8e7d0a09d3ef7a810
StepanUhryn <71820440+StepanUhryn@users.noreply.github.com>
2021-04-08 11:43:24 +0300

    Методи і властивості чисел та рядків

--- script/script.js
+++ script/script.js
@@ -1,42 +1,27 @@
 'use strict';
-/*
-
-let num = 20;
-function showFirstMessage(text) {
-    console.log(text);
-    //let num = 10;
-    console.log(num, 'локальна змінна');
-}
-showFirstMessage('Hello World!');
-console.log(num, 'глобальна змінна');
-
-
-function calc(a, b) {
-    return (a+b);
-}
-console.log(calc(4,5));
-console.log(calc(44,55));
-console.log(calc(14,15));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);*/
-
-
 
 /*
-const logger = function () {
-    console.log('Hello');
-}
-logger();*/
+const str = 'text text';
+
+console.log(str.length);
+
+const arr = [1, 2, 3, 4];
+console.log(arr.length);
+
+const fruit = 'Some Fruit';
+console.log(fruit.indexOf('Fruit'));
+
+const logg = 'Hello World!';
+console.log(logg.slice(6,11));*/
+
+/*
+const doggg = 'Hello World';
+console.log(doggg.substr(6, 5));*/
 
 
-const calc = (a, b) => {
-    console.log('hello boy');
-    return a + b;
-};
-console.log(calc(4, 6));
+/*  const s = 12.3;
+  console.log(Math.round(s));*/
+
+  const test = '12.2px';
+  console.log(parseInt(test));
+  console.log(parseFloat(test));
