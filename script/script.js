bd46080d21f5812f5a7108985957f31dfd1550bc
StepanUhryn <ugrinstepan@gmail.com>
2021-04-09 11:59:34 +0300

    Callback

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@
   setTimeout(function () {
     console.log('1');
   }, 500);
+
 }
 
 function second() {
@@ -15,10 +16,11 @@ first();
 second();*/
 
 function learnJS(lang, callback) {
-  console.log(`Я вчу ${lang}`);
-  callback();
+    console.log(`Я вчу ${lang}`);
+    callback();
 }
-learnJS('JavaScript', function () {
-  console.log('Я вивчив цей урок!')
-})
+function done() {
+  console.log('Я пройшов даний урок');
+}
 
+learnJS('JavaScript', done);
