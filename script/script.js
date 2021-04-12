37e681134db4707dfce3f8396e3b35fc340b8fd1
StepanUhryn <ugrinstepan@gmail.com>
2021-04-12 11:17:38 +0300

    Рекурсивна функція

--- script/script.js
+++ script/script.js
@@ -1,26 +1,15 @@
 'use strict';
 
-/*function first() {
-  //якісь дії
-  setTimeout(function () {
-    console.log('1');
-  }, 500);
 
+function calcSum(a, b) {
+    if (b === 1) {
+        return a;
+    } else {
+        //console.log(a * calcSum(a, b-1), 'всередині');
+        const rezult = calcSum(a, b-1);
+        console.log(rezult, 'результат дії функції');
+        return a * rezult;
+    }
 }
+console.log(calcSum(2, 3));
 
-function second() {
-  console.log('2');
-}
-
-first();
-second();*/
-
-function learnJS(lang, callback) {
-    console.log(`Я вчу ${lang}`);
-    callback();
-}
-function done() {
-  console.log('Я пройшов даний урок');
-}
-
-learnJS('JavaScript', done);
