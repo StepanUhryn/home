87d99f803a88dfc29d3ea2aab86db2c59c1d5369
stepan <ugrinstepan@gmail.com>
2021-04-14 11:28:45 +0300

    домашка - об'єкти

--- script/script.js
+++ script/script.js
@@ -1,115 +1,96 @@
 'use strict';
 
+// Task 1
 
-/*const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bgColor: 'red',
-    },
-    makeTest: function () {
-        console.log('Test');
-    }
-};
+/*const userInfo = {
+    name: 'Wasja',
+    age: 40
+}*/
 
-console.log(options.name, ', ', options['name']);*/
-
-/*delete options.name;
-
-console.log(options.name, ', ', options.width);
-console.log(options);*/
-
-
-/*let counter = 0;
-
-for (let key in options) {
-    /!*if (typeof options[key] === 'object') {
-        for (let i in options[key]) {
-            console.log(`Властивість ${i} має значення ${options[key][i]}`);
-            //counter++
-        }
-    } else {
-        console.log(`Властивість ${key} має значення ${options[key]}`);
-        counter++
-    }*!/
-    counter++
-}
-console.log(counter);*/
-
-//console.log(Object.keys(options).length);
-/*options.makeTest();
-
-const {border, bgColor} = options.colors;
-console.log(border, bgColor);*/
-
-/*
-function makeUserInfo(name, age) {
-    return {
-        name,
-        age
-    }
-}
-let user = makeUserInfo('Wasja', 30);
-let user_1 = makeUserInfo('Oleg', 35);
-console.log(user);
-console.log(user_1);*/
+// Task 2
 
 /*let userInfo = {
-    name: 'Василь'
+    name: 'Wasja',
+    age: 30,
+    58: 'Значення властивості'
 }
-//console.log(userInfo);
+console.log(userInfo[58]);*/
 
-userInfo.age = 30;
-//console.log(userInfo);
-
-userInfo['likes javascript'] = true;
-//console.log(userInfo);
-
-userInfo.adress = {
-    city: 'Uzhorod',
-    street: 'Sadova'
-};
-//console.log(userInfo)
-
-let user = Object.assign({}, userInfo);
-user.age = 20;
-
-console.log("об'єкт user ", user);
-console.log("об'єкт userInfo ",userInfo);*/
-
+// Task 3
 /*
+
+let userInfo = {
+    name: 'Wasja',
+    age: 30
+}
+let user = userInfo;
+user.age = 45;
+console.log(userInfo.age);
+console.log(user.age);*/
+
+
+// Task 4
+/*
+
 let userInfo = {
     name: 'Wasja',
     age: 30,
-    adress: {
-        city: 'Uzhorod',
-        street: 'Sadova'
-    },
-    showInfo: function () {
-        console.log(`${this.name}, ${this.age} років. Адреса: м. ${this.adress.city}, вул. ${this.adress.street}.`)
+    showInfo() {
+        console.log(`${this.name}`);
     }
-  /!*  showInfo() {
-    console.log(`${userInfo.name}, ${userInfo.age} років. Адреса: ${userInfo.adress.city}, вул. ${userInfo.adress.street}.`);
-    }*!/
 }
-userInfo.showInfo();*/
-//console.log("об'єкт userInfo ",userInfo);
-//console.log('Властивість street: ', userInfo.adress.street);
-//console.log(userInfo?.adress?.street);
+let user = userInfo;
+userInfo = null;
+console.log(userInfo);
+user.showInfo();*/
 
+/*
+// Task 5
+let userInfo = {
+    name: 'Wasja',
+    age: 30
+}
+for (const key in userInfo) {
+    const value = userInfo[key];
+    console.log(value);
+}
+*/
 
-/*if(userInfo.age) {
-    console.log('Властивість age: ',userInfo.age);
-} else {
-    console.log('Нема такої властивості');
+//Task 6
+/*
+
+let userInfo = {
+    name: 'Wasja',
+    age: 40,
+    adress: {
+        city: 'Uzhorod'
+    }
+}
+for (const key in userInfo.adress) {
+    console.log(userInfo.adress[key]);
 }*/
 
 
-function UserInfo(name) {
-    this.name = name;
-    this.age = 30;
+// Task 7
+/*
+
+const userInfo = {
+    name: 'Wasja',
+    age: 30,
+    'likes js': true
 }
-console.log(new UserInfo('Вася'));
-console.log(new UserInfo('Лена'));
+console.log(userInfo['likes js']);*/
+
+
+let dog = {};
+
+dog.name = 'Spiky';
+dog.age = 10;
+
+console.log(dog);
+
+dog.name = 'Liky';
+console.log(dog);
+
+delete dog.name;
+console.log(dog);
