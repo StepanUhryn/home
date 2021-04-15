258fddb341186feef6de67193b1f66fe8310b986
stepan <ugrinstepan@gmail.com>
2021-04-15 11:23:38 +0300

    Домашка рядки

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
 'use strict'
 
-/*let numOne = Math.round((1.005 + Number.EPSILON)*100) / 100;
-console.log(numOne);*/
-
-/*let value = "135.58px";
-let values = parseFloat("135.58px")
-console.log(values, typeof values)*/
 /*
+let fls = 'Фрілансер';
+/!*
+let dfd = `Привет! Я ${fls}`;
+console.log(dfd);*!/
 
-let value = 58 + 'Фрілансер';
-if(isNaN(value)) {
-    console.log('Результат виразу  NaN');
-} else {
-    console.log('Результат виразу - число.');
-}*/
+console.log(fls.substr(5, 1));
+console.log(fls[5]);*/
 
 
 /*
-console.log(Math.max(10, 58, 39, -150));
-console.log(Math.min(10, 58, 39, -150));*/
+let text = 123 + '456';
+console.log(text, typeof text);*/
 
 
+//let text = 'фрілансер';
+/*
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());*/
 
-console.log(58.896.toFixed(0));
-console.log(parseInt(58.858));
+//console.log(text.substr(3, 3));
+
+let text = 'Фрілансер';
+console.log(text.includes('лан', 4));
