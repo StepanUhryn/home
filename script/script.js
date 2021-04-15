ed9546c38cad7e6b1052d56dfdcd23edad3ed848
stepan <ugrinstepan@gmail.com>
2021-04-15 10:17:32 +0300

    Числа та їх методи

--- script/script.js
+++ script/script.js
@@ -17,9 +17,47 @@ console.log(+number.toFixed(1), typeof number);
 console.log(+number.toFixed(2), typeof number);
 */
 
-let num = 1.005 + Number.EPSILON;
+/*let num = 1.005 + Number.EPSILON;
 let num_1 = Math.round(num*100)/100;
 console.log(num_1);
 
 console.log(num*100);
-console.log(Math.round(num*100));
+console.log(Math.round(num*100));*/
+
+/*
+
+let col = 5.449;
+console.log(col.toFixed(1), typeof col.toFixed(1));
+console.log(+col.toFixed(1), typeof +col.toFixed(1));
+
+console.log(Number(col.toFixed(2)), typeof Number(col.toFixed(2)));*/
+
+
+/*
+let a = 0.2 + 0.1 === 0.3;
+console.log(a);
+console.log(0.2 + 0.1);*/
+
+
+/*
+console.log(Number(25 + 'Hi!'));
+console.log(isNaN(25 + 'Hi!'));
+
+console.log(NaN === NaN);*/
+
+
+/*
+console.log(isFinite(25));
+console.log(isFinite('Hi'));
+console.log(isFinite(10/0));*/
+
+
+/*
+let a = parseInt('1a50.58px');
+let b = parseFloat('150.58px');
+console.log(a, typeof a);
+console.log(b, typeof b);*/
+
+
+console.log(parseInt('0xFF', 16));
+console.log(parseInt('FF', 16));
