2153d4690b5f6c78365fd04c8ba73926e6f7489d
stepan <ugrinstepan@gmail.com>
2021-04-15 10:46:28 +0300

    Домашка числа

--- script/script.js
+++ script/script.js
@@ -1,63 +1,26 @@
-'use strict';
+'use strict'
 
-/*
-let number = Math.round(5.845*100)/100;
-console.log(number, typeof number);
+/*let numOne = Math.round((1.005 + Number.EPSILON)*100) / 100;
+console.log(numOne);*/
 
-let num = +number.toFixed(2);
-console.log(num, typeof num);*/
-/*
-
-console.log(number.toFixed(1), typeof number);
-console.log(number.toFixed(2), typeof number);
-*/
-
-/*
-console.log(+number.toFixed(1), typeof number);
-console.log(+number.toFixed(2), typeof number);
-*/
-
-/*let num = 1.005 + Number.EPSILON;
-let num_1 = Math.round(num*100)/100;
-console.log(num_1);
-
-console.log(num*100);
-console.log(Math.round(num*100));*/
-
+/*let value = "135.58px";
+let values = parseFloat("135.58px")
+console.log(values, typeof values)*/
 /*
 
-let col = 5.449;
-console.log(col.toFixed(1), typeof col.toFixed(1));
-console.log(+col.toFixed(1), typeof +col.toFixed(1));
-
-console.log(Number(col.toFixed(2)), typeof Number(col.toFixed(2)));*/
+let value = 58 + 'Фрілансер';
+if(isNaN(value)) {
+    console.log('Результат виразу  NaN');
+} else {
+    console.log('Результат виразу - число.');
+}*/
 
 
 /*
-let a = 0.2 + 0.1 === 0.3;
-console.log(a);
-console.log(0.2 + 0.1);*/
+console.log(Math.max(10, 58, 39, -150));
+console.log(Math.min(10, 58, 39, -150));*/
 
 
-/*
-console.log(Number(25 + 'Hi!'));
-console.log(isNaN(25 + 'Hi!'));
 
-console.log(NaN === NaN);*/
-
-
-/*
-console.log(isFinite(25));
-console.log(isFinite('Hi'));
-console.log(isFinite(10/0));*/
-
-
-/*
-let a = parseInt('1a50.58px');
-let b = parseFloat('150.58px');
-console.log(a, typeof a);
-console.log(b, typeof b);*/
-
-
-console.log(parseInt('0xFF', 16));
-console.log(parseInt('FF', 16));
+console.log(58.896.toFixed(0));
+console.log(parseInt(58.858));
