7113b93f48ea6fba652be8863faf06f43c758670
StepanUhryn <ugrinstepan@gmail.com>
2021-04-16 15:11:59 +0300

    Масиви

--- script/script.js
+++ script/script.js
@@ -1,26 +1,74 @@
 'use strict'
 
-/*
-let fls = 'Фрілансер';
-/!*
-let dfd = `Привет! Я ${fls}`;
-console.log(dfd);*!/
-
-console.log(fls.substr(5, 1));
-console.log(fls[5]);*/
-
 
 /*
-let text = 123 + '456';
-console.log(text, typeof text);*/
+let arr = [1, 32, 4, 77, -55, 90];
+console.log(arr);
+*/
 
+/*arr.pop();
+console.log(arr);
 
-//let text = 'фрілансер';
+arr.push(333, 88,666);
+console.log(arr);*/
+
+/*arr.shift();
+console.log(arr);*/
 /*
-console.log(text.toUpperCase());
-console.log(text.toLowerCase());*/
 
-//console.log(text.substr(3, 3));
+arr.unshift(0, .5);
+console.log(arr);
+*/
 
-let text = 'Фрілансер';
-console.log(text.includes('лан', 4));
+/*for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}*/
+//console.log(arr.length);
+/*for (let i of arr) {
+    console.log(i);
+}*/
+
+/*
+arr[10] = 0;
+console.log(arr.length);
+console.log(arr);
+*/
+/*
+arr.forEach(function (item, i, arr) {
+    console.log(`${i}: ${item} всередині масива ${arr}`);
+})*/
+
+/*
+const products = [5,4,3,2,1];
+products.sort(compareNu);
+console.log(products);
+
+function compareNu(a, b) {
+    return a-b;
+}
+*/
+
+/*
+const str = 'Wasja, Kolja, Olja, Igor';
+const arr = str.split(', ');
+
+const arr_map = arr.map(function(elementName, index, array) {
+    return elementName[4];
+});
+
+console.log(arr);
+console.log(arr_map);*/
+
+/*let a = {};
+let b = [];
+
+console.log('a :', typeof a);
+console.log('b :', typeof b);
+
+if (Array.isArray(b)) {
+    console.log('massiv');
+}   else {
+    console.log('not massiv');
+}*/
+
+
